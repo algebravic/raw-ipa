1e217c2fd4ab37b97dffdbdbd630dc45f692c991
Victor S. Miller <algebravic@fb.com>
2022-08-30 11:38:45 -0700

    Added imports from Compiler and proper qualification

--- research-prototype/Programs/Source/extra.py
+++ research-prototype/Programs/Source/extra.py
@@ -1,11 +1,15 @@
 """
 Extra debugging routines.
 """
+from Compiler import types, library
+from types import Tuple
+
 def print_property(fun, vec, msg):
 
-    print_ln("%s: %s", msg, tree_reduce(operator.add,fun(vec)).reveal())
+    print_ln("%s: %s", msg,
+             library.tree_reduce(operator.add, fun(vec)).reveal())
 
-def reconstruct(bit_array: Matrix) -> Array:
+def reconstruct(bit_array: types.Matrix) -> types.Array:
     """
     bit_array is an N by B array of bits
     compute the integer representation.
@@ -13,13 +17,14 @@ def reconstruct(bit_array: Matrix) -> Array:
     """
 
     num, n_bits = bit_array.sizes
-    result = Array(num, sint)
+    result = types.Array(num, sint)
     result.assign_vector(bit_array.get_column(n_bits - 1))
+
     for ind in range(n_bits - 2, -1, -1):
         result = result + result + bit_array.get_column(ind)
     return result
 
-def validate_bits(arr: Array) -> Tuple[sint, sint]:
+def validate_bits(arr: types.Array) -> Tuple[types.sint, types.sint]:
     """
     Test a purported bit vector.
     """
@@ -27,6 +32,6 @@ def validate_bits(arr: Array) -> Tuple[sint, sint]:
     small = arr.get_vector() < 0
     big = arr.get_vector() > 1
 
-    return (tree_reduce(operator.add, small),
-            tree_reduce(operator.add, big))
+    return (library.tree_reduce(operator.add, small),
+            library.tree_reduce(operator.add, big))
 
